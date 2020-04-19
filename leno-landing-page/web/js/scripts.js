dc400fdc9424c219839e6157323a9ce4ace02e9f
Zach Goldberg <zach.goldberg@inventive.io>
2020-04-18 23:39:19 -0500

    merge to master

--- leno-landing-page/web/js/scripts.js
+++ leno-landing-page/web/js/scripts.js
@@ -262,63 +262,64 @@
         }
       });
 
-    function csubmitForm() {
-      // initiate variables with form content
-      var name = $("#cname").val();
-      var email = $("#cemail").val();
-      var message = $("#cmessage").val();
-      // var terms = $("#cterms").val();
-      $.ajax({
-        type: "POST",
-        url: "../php/contactform-process.php",
-        data:
-          "name=" +
-          name +
-          "&email=" +
-          email +
-          "&message=" +
-          message,
-        success: function(text) {
-          if (text == "success") {
-            cformSuccess();
-          } else {
-            cformError();
-            csubmitMSG(false, text);
-          }
-        }
-      });
-    }
+    // function csubmitForm() {
+    //   // initiate variables with form content
+    //   var name = $("#cname").val();
+    //   var email = $("#cemail").val();
+    //   var message = $("#cmessage").val();
+    //   // var terms = $("#cterms").val();
+    //   console.log(name, email, message);
+    //   $.ajax({
+    //     type: "POST",
+    //     url: "php/contactform-process.php",
+    //     data:
+    //       "name=" +
+    //       name +
+    //       "&email=" +
+    //       email +
+    //       "&message=" +
+    //       message,
+    //     success: function(text) {
+    //       if (text == "success") {
+    //         cformSuccess();
+    //       } else {
+    //         cformError();
+    //         csubmitMSG(false, text);
+    //       }
+    //     }
+    //   });
+    // }
 
-    function cformSuccess() {
-      $("#contactForm")[0].reset();
-      csubmitMSG(true, "Message Submitted!");
-      $("input").removeClass("notEmpty"); // resets the field label after submission
-      $("textarea").removeClass("notEmpty"); // resets the field label after submission
-    }
+    // function cformSuccess() {
+    //   $("#contactForm")[0].reset();
+    //   csubmitMSG(true, "Message Submitted!");
+    //   $("input").removeClass("notEmpty"); // resets the field label after submission
+    //   $("textarea").removeClass("notEmpty"); // resets the field label after submission
+    // }
 
-    function cformError() {
-      $("#contactForm")
-        .removeClass()
-        .addClass("shake animated")
-        .one(
-          "webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend",
-          function() {
-            $(this).removeClass();
-          }
-        );
-    }
+    // function cformError() {
+    //   $("#contactForm")
+    //     .removeClass()
+    //     .addClass("shake animated")
+    //     .one(
+    //       "webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend",
+    //       function() {
+    //         $(this).removeClass();
+    //       }
+    //     );
+    // }
 
-    function csubmitMSG(valid, msg) {
-      if (valid) {
-        var msgClasses = "h3 text-center tada animated";
-      } else {
-        var msgClasses = "h3 text-center";
-      }
-      $("#cmsgSubmit")
-        .removeClass()
-        .addClass(msgClasses)
-        .text(msg);
-    }
+    // function csubmitMSG(valid, msg) {
+    //   if (valid) {
+    //     var msgClasses = "h3 text-center tada animated";
+    //   } else {
+    //     var msgClasses = "h3 text-center";
+    //   }
+    //   $("#cmsgSubmit")
+    //     .removeClass()
+    //     .addClass(msgClasses)
+    //     .text(msg);
+    // }
 
   //   /* Privacy Form */
   //   $("#privacyForm")
